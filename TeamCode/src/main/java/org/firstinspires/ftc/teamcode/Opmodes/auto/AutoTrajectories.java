04d0f3a0c0992aef6a5178697495acf3bdb1f740
Henry51s <quochenry06@gmail.com>
2024-02-02 22:57:32 -0500

    aughaiugfaeiufgaeiubfg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/auto/AutoTrajectories.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/auto/AutoTrajectories.java
@@ -38,23 +38,41 @@ public class AutoTrajectories {
     public AutoTrajectories(HardwareMap hw){
         drive = new SampleMecanumDrive(hw);
 
-        }
-    public static double x1 = 49, y1 = -45, heading1 = 10;
-
+    }
     public Vector2d  poseToVector(Pose2d pose){
         Vector2d result = new Vector2d(pose.getX(), pose.getY());
         return result;
-}
-    public double getPoseHeading(Pose2d pose){
-        return pose.getHeading();
     }
 
 
     public static double neutral1X = -37.5, neutral1Y = 57, neutral1Heading = 0;
     public static double scoringX = 52.5, scoringY = 18.175  , scoringHeading = 0;
 
-    public Pose2d neutralPose1 = new Pose2d(neutral1X, neutral1Y, Math.toRadians(neutral1Heading));
-    public Pose2d scoringPose = new Pose2d(scoringX, scoringY, Math.toRadians(scoringHeading));
+
+    /*public static double spikeMarkLX = 0, spikeMarkLY = 0, spikeMarkLHeading = 0;
+    public static double spikeMarkMX = 0, spikeMarkMY = 0, spikeMarkMHeading = 0;
+    public static double spikeMarkRX = 0, spikeMarkRY = 0, spikeMarkRHeading = 0;*/
+
+    public static double[] spikeMarkL = {0,0,0};
+    public static double[] spikeMarkM = {0,0,0};
+    public static double[] spikeMarkR = {0,0,0};
+
+    public static double[] backDropL = {0,0,0};
+    public static double[] backDropM = {0,0,0};
+    public static double[] backDropR = {0,0,0};
+
+    Pose2d poseSpikeMarkL = new Pose2d(spikeMarkL[0], spikeMarkL[1], Math.toRadians(spikeMarkL[2]));
+    Pose2d poseSpikeMarkM = new Pose2d(spikeMarkM[0], spikeMarkL[1], Math.toRadians(spikeMarkM[2]));
+    Pose2d poseSpikemarkR = new Pose2d(spikeMarkR[0], spikeMarkR[1], Math.toRadians(spikeMarkR[2]));
+
+    Pose2d poseBackDropL = new Pose2d(backDropL[0], backDropL[1], Math.toRadians(backDropL[2]));
+    Pose2d poseBackDropM = new Pose2d(backDropM[0], backDropM[1], Math.toRadians(backDropM[2]));
+    Pose2d poseBackDropR = new Pose2d(backDropR[0], backDropR[1], Math.toRadians(backDropR[2]));
+
+
+    Pose2d neutralPose1 = new Pose2d(neutral1X, neutral1Y, Math.toRadians(neutral1Heading));
+    Pose2d scoringPose = new Pose2d(scoringX, scoringY, Math.toRadians(scoringHeading));
+
 
 
 
@@ -120,7 +138,7 @@ public class AutoTrajectories {
 
                     toNeutral = drive.trajectorySequenceBuilder(backupSpikeMark.end())
                             .lineToConstantHeading(new Vector2d(-40, 54))
-                            .turn(Math.toRadians(-(180 + neutral1Heading)))
+                            .turn(Math.toRadians(-180))
                             .build();
 
                     scoreBackDrop = drive.trajectorySequenceBuilder(new Pose2d(-40, 54, Math.toRadians(0)))
@@ -193,7 +211,7 @@ public class AutoTrajectories {
 
                     toNeutral = drive.trajectorySequenceBuilder(backupSpikeMark.end())
                             .lineToConstantHeading(poseToVector(neutralPose1))
-                            .turn(Math.toRadians(180 + neutral1Heading))
+                            .turn(Math.toRadians(180))
                             .build();
 
                     scoreBackDrop = drive.trajectorySequenceBuilder(new Pose2d(-37.5, -57, Math.toRadians(0)))
@@ -217,22 +235,8 @@ public class AutoTrajectories {
 
 
                 if(spikeMark == SpikeMark.LEFT){
-                    /*scoreSpikeMark = drive.trajectorySequenceBuilder(startPose)
-                            .setReversed(true)
-                            .lineToLinearHeading(new Pose2d(20,30, Math.toRadians(0)))
-                            .back(5)
-                            .build();
-                    scoreBackDrop = drive.trajectorySequenceBuilder(scoreSpikeMark.end())
-                            .setReversed(true)
-                            .lineToConstantHeading(new Vector2d(55, 22))
-                            .waitSeconds(0.5)
-                            .build();*/
                     scoreSpikeMark = drive.trajectorySequenceBuilder(startPose)
                             .setReversed(true)
-                            //.lineToLinearHeading(new Pose2d(34, -40, Math.toRadians(-90)))
-                            //.splineToConstantHeading(new Vector2d(28,-40), Math.toRadians(-90))
-                            //.splineTo(new Vector2d(32,-40), Math.toRadians(-90))
-                            //.splineToLinearHeading(new Pose2d(32,-40, Math.toRadians(-90)), Math.toRadians(-90))
                             .splineToConstantHeading(new Vector2d(28, 40), Math.toRadians(0))
                             .build();
                     scoreBackDrop = drive.trajectorySequenceBuilder(scoreSpikeMark.end())
@@ -287,7 +291,7 @@ public class AutoTrajectories {
                             .waitSeconds(0.5)
                             .build();
                     park = drive.trajectorySequenceBuilder(scoreBackDrop.end())
-                            .lineToConstantHeading(new Vector2d(x1, -11))
+                            .lineToConstantHeading(new Vector2d(49, -11))
                             .build();
 
                 }
@@ -302,7 +306,7 @@ public class AutoTrajectories {
                             .waitSeconds(0.5)
                             .build();
                     park = drive.trajectorySequenceBuilder(scoreBackDrop.end())
-                            .lineToConstantHeading(new Vector2d(x1, -55))
+                            .lineToConstantHeading(new Vector2d(49, -55))
                             .build();
 
 
@@ -322,7 +326,7 @@ public class AutoTrajectories {
                             .waitSeconds(0.5)
                             .build();
                     park = drive.trajectorySequenceBuilder(scoreBackDrop.end())
-                            .lineToConstantHeading(new Vector2d(x1, -55))
+                            .lineToConstantHeading(new Vector2d(49, -55))
                             .build();
                 }
                 break;
