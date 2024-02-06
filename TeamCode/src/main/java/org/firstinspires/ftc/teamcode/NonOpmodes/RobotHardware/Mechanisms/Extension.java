43c70f89931706f50425721364c0e3ba42624a78
Henry <quochenry06@gmail.com>
2024-02-06 01:20:19 -0500

    Implemented custom PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/RobotHardware/Mechanisms/Extension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/RobotHardware/Mechanisms/Extension.java
@@ -9,11 +9,13 @@ import static org.firstinspires.ftc.teamcode.NonOpmodes.RobotHardware.Globals.Gl
 import static org.firstinspires.ftc.teamcode.NonOpmodes.RobotHardware.Globals.GlobalVars.EXTENDO_SHORT;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.NonOpmodes.RobotHardware.Globals.Hardware;
 
 @Config
@@ -26,8 +28,23 @@ public class Extension {
         }
         return instance;
     }
-    private int targetPosition = 0;
 
+    public static double p = 0;
+    public static double i = 0;
+    public static double d = 0;
+    public static double positionTolerance = 0;
+    public static double joystickMultiplier = 5;
+    PIDController pid = new PIDController(p,i,d);
+
+
+
+
+
+
+
+    private int targetPosition = 0;
+    private double currentThreshold = 0;
+    private int homePosition = 0;
     private DcMotorEx extendoL, extendoR;
     private Hardware hardware = new Hardware();
 
@@ -46,12 +63,16 @@ public class Extension {
         extendoL = hardware.extendoL;
         extendoR = hardware.extendoR;
 
-        extendoL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        /*extendoL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         extendoR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         setTargetPosition(0);
         extendoL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         extendoR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        setPower(EXTENDO_MAX_POWER);
+        setPower(EXTENDO_MAX_POWER);*/
+
+        extendoL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        extendoR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        pid.setTolerance(positionTolerance);
 
         targetPosition = 0;
     }
@@ -60,6 +81,20 @@ public class Extension {
         extendoL = hardware.extendoL;
         extendoR = hardware.extendoR;
     }
+
+    private double calculateVel(){
+        double vel = pid.calculate(
+                getAveragePosition(), targetPosition
+        );
+        return vel;
+    }
+
+    private void setVelocity(double velocity){
+        extendoL.setVelocity(velocity);
+        extendoR.setVelocity(velocity);
+    }
+
+
     public void setPower(double power){
         extendoL.setPower(power);
         extendoR.setPower(power);
@@ -94,22 +129,40 @@ public class Extension {
 
     public void loopExtension(Gamepad gamepad){
 
+
+
         previous.copy(current);
         current.copy(gamepad);
-        targetPosition += -gamepad.left_stick_y*5 ;
-        targetPosition = Math.max(0, Math.min(targetPosition, EXTENDO_FAR));
+        targetPosition += -gamepad.left_stick_y*joystickMultiplier;
+        targetPosition = Math.max(0, Math.min(targetPosition, homePosition + EXTENDO_FAR));
 
         if(current.left_stick_button && !previous.left_stick_button){
-            targetPosition = 0;
+            targetPosition = homePosition;
         }
 
         setTargetPosition(targetPosition);
+        double output = calculateVel();
+        setVelocity(output);
+
+        if(getAverageCurrent(CurrentUnit.AMPS) > currentThreshold && calculateVel() < 0){
+            setVelocity(0);
+            homePosition = getAveragePosition();
+            targetPosition = homePosition;
+
+        }
 
     }
     public int getMotorLCurrentPosition(){return extendoL.getCurrentPosition();}
     public int getMotorRCurrentPosition(){
         return extendoR.getCurrentPosition();
     }
+    public int getAveragePosition(){
+        return (getMotorLCurrentPosition() + getMotorRCurrentPosition())/2;
+    }
+    public double getAverageCurrent(CurrentUnit currentUnit){
+        return (extendoL.getCurrent(currentUnit) + extendoR.getCurrent(currentUnit))/2;
+    }
+
     public int getTargetPosition(){
         return targetPosition;
     }
