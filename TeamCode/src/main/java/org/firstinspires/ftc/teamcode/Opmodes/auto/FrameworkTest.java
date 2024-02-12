7c61a09a59de4e58f6a17506f0aa69e051fb2afe
Henry51s <quochenry06@gmail.com>
2024-02-11 22:57:16 -0500

    Introduced AutoFramework System

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/auto/FrameworkTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.ejml.equation.Operation;
+
+public class FrameworkTest extends OpMode {
+    ShortSideFramework shortSideFramework = new ShortSideFramework();
+    @Override
+    public void init() {
+        shortSideFramework.init();
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/auto/ShortSideFramework.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class ShortSideFramework extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
